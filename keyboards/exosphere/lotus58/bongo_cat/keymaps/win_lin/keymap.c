e07f5cec19dd668d230455ef4def00df319867c7
sdrauksas <71008456+sdrauksas@users.noreply.github.com>
2023-05-21 19:06:23 +0300

    keymap tweaks

--- keyboards/exosphere/lotus58/bongo_cat/keymaps/win_lin/keymap.c
+++ keyboards/exosphere/lotus58/bongo_cat/keymaps/win_lin/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RSFT,
     KC_LGUI, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_VOLD,       KC_VOLU, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RGUI,
-                               KC_LALT, MO(_NAV),KC_LCTL, KC_ENT,        KC_SPC,  KC_RCTL, MO(_SYM),KC_RALT
+                               KC_LALT, MO(_SYM),KC_LCTL, KC_ENT,        KC_SPC,  KC_RCTL, MO(_NAV),KC_RALT
 ),
 
 [_GAM] = LAYOUT(
@@ -53,22 +53,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RSFT,
     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_VOLD,       KC_VOLU, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RGUI,
-                               KC_LALT, MO(_NAV),KC_SPC,  KC_ENT,        DF(_QWE),KC_RCTL, MO(_SYM),KC_RALT
+                               KC_LALT, MO(_SYM),KC_SPC,  KC_ENT,        DF(_QWE),KC_RCTL, MO(_NAV),KC_RALT
 ),
 
 [_SYM] = LAYOUT(
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______,       _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-    _______, XXXXXXX, KC_GRV,  KC_MINS, KC_EQL,  XXXXXXX,                         XXXXXXX, KC_LBRC, KC_RBRC, KC_QUOT, KC_BSLS, _______,
-    _______, DF(_GAM),XXXXXXX, XXXXXXX, XXXXXXX, CH_LANG, _______,       _______, PR_SCR,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+    _______, KC_BSLS, KC_QUOT, KC_LBRC, KC_RBRC, XXXXXXX,                         XXXXXXX, KC_P4,   KC_P5,   KC_P6,   KC_PAST, _______,
+    _______, XXXXXXX, KC_GRV,  KC_MINS, KC_EQL,  XXXXXXX,                         XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_PMNS, _______,
+    _______, DF(_GAM),XXXXXXX, XXXXXXX, KC_PSCR, CH_LANG, _______,       _______, XXXXXXX, KC_PDOT, KC_P0,   KC_PEQL, KC_PPLS, _______,
                                _______, _______, _______, _______,       _______, _______, _______, _______
 ),
 
 [_NAV] = LAYOUT(
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPLY,       _______, XXXXXXX, KC_P7,   KC_P8,   KC_P9,   KC_PSLS, _______,
-    _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, XXXXXXX,                         XXXXXXX, KC_P4,   KC_P5,   KC_P6,   KC_PAST, _______,
-    _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, XXXXXXX,                         XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_PMNS, _______,
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPRV,       KC_MNXT, XXXXXXX, KC_PDOT, KC_P0,   KC_PEQL, KC_PPLS, _______,
+    _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, XXXXXXX,                         XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, XXXXXXX,                         XXXXXXX, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPRV,       KC_MNXT, XXXXXXX, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
                                _______, _______, _______, _______,       _______, _______, _______, _______
 )
 };
--- keyboards/exosphere/lotus58/with_leds/keymaps/win_lin/keymap.c
+++ keyboards/exosphere/lotus58/with_leds/keymaps/win_lin/keymap.c
@@ -3,9 +3,10 @@
 #include QMK_KEYBOARD_H
 
 enum layers {
-    _QWERTY,
-    _GAME,
-    _SYM
+    _QWE,
+    _GAM,
+    _SYM,
+    _NAV
 };
 
 enum custom_keycodes {
@@ -44,29 +45,38 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[_QWERTY] = LAYOUT(
-      KC_ESC, 	 KC_1, 	    KC_2,      KC_3,	  KC_4,      KC_5,  	KC_KB_MUTE,    KC_NO,      KC_6,  	 KC_7,   	KC_8,      KC_9,      KC_0,      KC_DEL,
-      KC_TAB, 	 KC_Q, 	    KC_W,      KC_E,      KC_R,      KC_T,                     		       KC_Y,     KC_U,      KC_I,      KC_O,      KC_P,      KC_BSPC,
-      KC_LSFT, 	 KC_A, 	    KC_S,      KC_D,      KC_F,      KC_G,                     			   KC_H, 	 KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_RSFT,
-      KC_LGUI,   KC_Z, 	    KC_X,      KC_C,      KC_V,      KC_B,      KC_VOLD,       KC_VOLU,    KC_N,	 KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,   KC_RGUI,
-                                       KC_LALT,   MO(_SYM),  KC_LCTL,   KC_ENT,        KC_SPC,     KC_RCTL,  MO(_SYM),  KC_RALT
+[_QWE] = LAYOUT(
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,       XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RSFT,
+    KC_LGUI, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_VOLD,       KC_VOLU, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RGUI,
+                               KC_LALT, MO(_SYM),KC_LCTL, KC_ENT,        KC_SPC,  KC_RCTL, MO(_NAV),KC_RALT
 ),
 
-[_GAME] = LAYOUT(
-      KC_ESC, 	 KC_1, 	    KC_2,      KC_3,	  KC_4,      KC_5,  	KC_KB_MUTE,    KC_NO,      KC_6,  	 KC_7,   	KC_8,      KC_9,      KC_0,      KC_DEL,
-      KC_TAB, 	 KC_Q, 	    KC_W,      KC_E,      KC_R,      KC_T,                     		       KC_Y,     KC_U,      KC_I,      KC_O,      KC_P,      KC_BSPC,
-      KC_LSFT, 	 KC_A, 	    KC_S,      KC_D,      KC_F,      KC_G,                     			   KC_H, 	 KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_RSFT,
-      KC_LCTL,   KC_Z, 	    KC_X,      KC_C,      KC_V,      KC_B,      KC_VOLD,       KC_VOLU,    KC_N,	 KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,   KC_RGUI,
-                                       KC_LALT,   MO(_SYM),  KC_SPC,    KC_ENT,        KC_SPC,     KC_RCTL,  DF(_QWERTY),KC_RALT
+[_GAM] = LAYOUT(
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,      XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RSFT,
+    KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_VOLD,       KC_VOLU, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
+                               KC_LALT, MO(_SYM),KC_SPC,  KC_ENT,        DF(_QWE),KC_RGUI, MO(_NAV),KC_RALT
 ),
 
 [_SYM] = LAYOUT(
-      _______, 	 KC_F2,	    KC_F3,     KC_F4,	  KC_F5,     KC_F6,  	KC_MPLY,       KC_NO,      KC_F7,  	 KC_F8,   	KC_F9,     KC_F10,    KC_F11,    _______,
-      _______, 	 KC_F1, 	KC_UP,     KC_GRV,    KC_MINS,   KC_EQL,                   		       KC_LBRC,  KC_RBRC,   KC_QUOT,   KC_MS_U,   KC_F12,    _______,
-      _______, 	 KC_LEFT,   KC_DOWN,   KC_RGHT,   S(KC_MINS),S(KC_EQL),                     	   S(KC_LBRC),S(KC_RBRC),KC_MS_L,  KC_MS_D,   KC_MS_R,   _______,
-      _______,   DF(_GAME), RGB_TOG,   RGB_MOD,   RGB_VAD,   CH_LANG,   KC_MPRV,       KC_MNXT,    PR_SCR,	 KC_BSLS,   KC_BTN1,   KC_BTN3,   KC_BTN2,   _______,
-                                       _______,   _______,   _______,   _______,       _______,    _______,  _______,   _______
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+    _______, KC_BSLS, KC_QUOT, KC_LBRC, KC_RBRC, XXXXXXX,                         XXXXXXX, KC_P4,   KC_P5,   KC_P6,   KC_PAST, _______,
+    _______, XXXXXXX, KC_GRV,  KC_MINS, KC_EQL,  XXXXXXX,                         XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_PMNS, _______,
+    _______, DF(_GAM),XXXXXXX, XXXXXXX, KC_PSCR, CH_LANG, KC_MPLY,       KC_KB_MUTE,XXXXXXX,KC_PDOT,KC_P0,   KC_PEQL, KC_PPLS, _______,
+                               _______, _______, _______, _______,       _______, _______, _______, _______
 ),
+
+[_NAV] = LAYOUT(
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,       _______, XXXXXXX, KC_P7,   KC_P8,   KC_P9,   KC_PSLS, _______,
+    _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, XXXXXXX,                         XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, XXXXXXX,                         XXXXXXX, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,
+    _______, RGB_TOG, RGB_MOD, RGB_VAD, XXXXXXX, XXXXXXX, KC_MPRV,       KC_MNXT, XXXXXXX, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
+                               _______, _______, _______, _______,       _______, _______, _______, _______
+)
+
 };
 
 // RGB
@@ -85,18 +95,26 @@ bool rgb_matrix_indicators_kb(void) {
     red_scaled_hsv.v = val;
     RGB red_scaled_rgb;
 
+    HSV green_scaled_hsv = { HSV_GREEN };
+    green_scaled_hsv.v = val;
+    RGB green_scaled_rgb;
+
     switch(get_highest_layer(layer_state | default_layer_state)) {
-        case _QWERTY:
+        case _QWE:
             white_scaled_rgb = hsv_to_rgb(white_scaled_hsv);
             rgb_matrix_set_color_all(white_scaled_rgb.r, white_scaled_rgb.g, white_scaled_rgb.b); // QWERTY
             break;
-        case _GAME:
+        case _GAM:
             blue_scaled_rgb = hsv_to_rgb(blue_scaled_hsv);
             rgb_matrix_set_color_all(blue_scaled_rgb.r, blue_scaled_rgb.g, blue_scaled_rgb.b); // GAME
             break;
         case _SYM:
             red_scaled_rgb = hsv_to_rgb(red_scaled_hsv);
             rgb_matrix_set_color_all(red_scaled_rgb.r, red_scaled_rgb.g, red_scaled_rgb.b); // RAISE
+            break;
+        case _NAV:
+            green_scaled_rgb = hsv_to_rgb(green_scaled_hsv);
+            rgb_matrix_set_color_all(green_scaled_rgb.r, green_scaled_rgb.g, green_scaled_rgb.b); // NAV
             break;
         default:
             break;
