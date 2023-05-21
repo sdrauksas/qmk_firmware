704bff54c685c2d6af6dff631bc6399ea380c79b
sdrauksas <71008456+sdrauksas@users.noreply.github.com>
2023-05-21 20:44:46 +0300

    fix numpad

--- keyboards/exosphere/lotus58/bongo_cat/keymaps/mac/keymap.c
+++ keyboards/exosphere/lotus58/bongo_cat/keymaps/mac/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_SYM] = LAYOUT(
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,       _______, XXXXXXX, KC_P7,   KC_P8,   KC_P9,   KC_PSLS, _______,
     _______, KC_BSLS, KC_QUOT, KC_LBRC, KC_RBRC, XXXXXXX,                         XXXXXXX, KC_P4,   KC_P5,   KC_P6,   KC_PAST, _______,
     _______, XXXXXXX, KC_GRV,  KC_MINS, KC_EQL,  XXXXXXX,                         XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_PMNS, _______,
     _______, DF(_GAM),XXXXXXX, XXXXXXX, PR_SCR,  CH_LANG, _______,       _______, XXXXXXX, KC_PDOT, KC_P0,   KC_PEQL, KC_PPLS, _______,
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_NAV] = LAYOUT(
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPLY,       _______, XXXXXXX, KC_P7,   KC_P8,   KC_P9,   KC_PSLS, _______,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPLY,       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
     _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, XXXXXXX,                         XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,
     _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, XXXXXXX,                         XXXXXXX, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPRV,       KC_MNXT, XXXXXXX, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
